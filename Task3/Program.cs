79133f28b40e79d0df6120e418ef1f606a260029
Dmitry Vasiliuk <usy222@mail.ru>
2023-02-20 22:39:21 +0300

    Таблица Кубов Числа N

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая на вход принимает число (N) и выдает
+// таблицу кубов чисел от 1 до (N): _Задача 23_
+
+Console.Write("Введите число: ");
+
+int.TryParse(Console.ReadLine(), out int N);
+int index = 1;
+
+while (index <= N)
+{
+  Console.Write(Math.Pow((index), 3) + " ");
+  index++;
+}
