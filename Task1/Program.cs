098b16769c58c88b8e2d66670798d53cbf097d8b
Dmitry Vasiliuk <usy222@mail.ru>
2023-02-21 00:22:49 +0300

    Выясняем, является ли число палиндромом

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Программа, которая принимает на вход пятизначное число и проверяет,
+//является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число: ");
+int.TryParse(Console.ReadLine(), out int first);
+int length = first.ToString().Length;
+
+if (length == 5)
+{
+  int a = first % 100;
+  int b = first / 1000;
+  int c = first / 100 % 10;
+  
+  if (b.ToString()[0] == a.ToString()[1] && b.ToString()[1] == a.ToString()[0])
+  {
+    Console.WriteLine($"Введенное Вами число {first} является - палиндромом: {b} {c} {a}");
+  }
+  else
+  {
+    Console.WriteLine($"Вы ввели число {first}, оно НЕ является палиндромом: {b} {c} {a}");
+  }
+}
+else
+{
+  Console.Write($"Число {first} - НЕ является пятизначным, повторите ввод числа!");
+}
